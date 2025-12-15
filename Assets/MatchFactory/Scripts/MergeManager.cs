da2e8d06959f861dd8905dbb701e0958e7d19028
hasanaliozdemir <hasan.aliozdemir10@gmail.com>
2025-12-15 09:36:57 +0300

    Particle assets and merge animation

--- Assets/MatchFactory/Scripts/MergeManager.cs
+++ Assets/MatchFactory/Scripts/MergeManager.cs
@@ -6,11 +6,17 @@ using UnityEngine;
 public class MergeManager : MonoBehaviour
 {
 
-    [Header("Settings")]
+    [Header("Go Up Settings")]
     [SerializeField] private float goUpDistance = 0.15f;
     [SerializeField] private float goUpDuration = 0.15f;
     [SerializeField] private LeanTweenType goUpEasing = LeanTweenType.easeInOutSine;
-    [SerializeField] private float mergeDuration = 0.3f;
+
+    [Header("Smash Settings")]
+    [SerializeField] private float smashDuration = 0.3f;
+    [SerializeField] private LeanTweenType smashEasing = LeanTweenType.easeInOutSine;
+
+    [Header("Effects")]
+    [SerializeField] private ParticleSystem mergeParticles;
 
     void Awake()
     {
@@ -46,10 +52,30 @@ public class MergeManager : MonoBehaviour
 
     private void SmashItems(List<Item> items)
     {
+        // Sorting left to right
+        items.Sort((a, b) => a.transform.position.x.CompareTo(b.transform.position.x));
+
+        float targetX = items[1].transform.position.x;
+
+        LeanTween.moveX(items[0].gameObject, targetX, smashDuration).setEase(smashEasing).setOnComplete(() =>
+        {
+            FinalizeMerge(items);
+        });
+
+        LeanTween.moveX(items[2].gameObject, targetX, smashDuration).setEase(smashEasing);
+
+
+    }
+
+    private void FinalizeMerge(List<Item> items)
+    {
         for (int i = 0; i < items.Count; i++)
         {
             Destroy(items[i].gameObject);
         }
+
+        ParticleSystem particles = Instantiate(mergeParticles, items[1].transform.position, Quaternion.identity, transform);
+        particles.Play();
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
