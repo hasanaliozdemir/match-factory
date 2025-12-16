171b327b45630e4f8d5f1439f5166dda78d758e5
hasanaliozdemir <hasan.aliozdemir10@gmail.com>
2025-12-16 13:44:34 +0300

    Goal Card Animations

--- Assets/MatchFactory/Scripts/UI/GoalCard.cs
+++ Assets/MatchFactory/Scripts/UI/GoalCard.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,18 +9,21 @@ public class GoalCard : MonoBehaviour
     [Header("Elements")]
     [SerializeField] private TextMeshProUGUI amountText;
     [SerializeField] private Image iconImage;
+    [SerializeField] private GameObject checkmark;
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject backFace;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        animator.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        backFace.SetActive(Vector3.Dot(Vector3.forward, transform.forward) < 0);
     }
 
     public void Configure(int initialAmount, Sprite icon)
@@ -32,10 +36,28 @@ public class GoalCard : MonoBehaviour
     public void UpdateAmount(int newAmount)
     {
         amountText.text = newAmount.ToString();
+
+        BumpAnimation();
+    }
+
+    private void BumpAnimation()
+    {
+        // Cancel any ongoing animations
+        LeanTween.cancel(gameObject);
+        transform.localScale = Vector3.one;
+
+        LeanTween.scale(gameObject, Vector3.one * 1.2f, 0.25f).setLoopPingPong(1);
     }
 
     public void Complete()
     {
-        gameObject.SetActive(false);
+        // gameObject.SetActive(false);
+
+        animator.enabled = true;
+
+        checkmark.SetActive(true);
+        amountText.gameObject.SetActive(false);
+
+        animator.Play("Complete");
     }
 }
